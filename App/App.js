bd9281ebd279e2018d9529f3ccf2a4532b1dda1f
sr0409 <sr0409@live.missouristate.edu>
2019-05-29 18:13:53 -0500

    Started to work on the menu

---
+++ App/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { AppRegistry, FlatList, StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default class FlatListBasics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'BUILD'},
+            {key: 'PART SEARCH'},
+            {key: 'ABOUT'} 
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22,
+   alignItems: 'stretch',
+   flexWrap: 'wrap'
+   
+  },
+  item: {
+    //alignSelf: 'center',
+    //flexGrow: 1,
+    margin: 10,
+    backgroundColor: 'red',
+    fontSize: 18,
+    textAlign: 'center',
+    alignSelf: 'stretch',
+
+    },
+})
