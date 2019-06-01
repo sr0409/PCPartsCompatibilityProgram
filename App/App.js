6ce577ae440dc9c6a3538ffad01571c5738667a3
User <sr0876392@otc.edu>
2019-05-31 21:38:53 -0500

    buttons had to be replaced with a nav menu, works fine

--- App/App.js
+++ App/App.js
@@ -1,33 +1,86 @@
-import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet, AppRegistry } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation'; 
 
-export default class FlatListBasics extends Component {
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
   render() {
     return (
       <View style={styles.container}>
-        
-          <Text style={styles.item}>Build</Text>
-          <Text style={styles.item}>Part search</Text>
-          <Text style={styles.item}>About</Text>
-
+        <TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Details')}>
+          <Text style={styles.item}>Click Me 111111!</Text>
+        </TouchableOpacity>
+        <TouchableOpacity  style={styles.item} onPress={() => this.props.navigation.navigate('Details')}>
+          <Text style={styles.item}>Click Me!22222222</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Details')}>
+          <Text style={styles.item}>Click Me!3333333</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Details Screen</Text>
+        <TouchableOpacity
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <TouchableOpacity
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <TouchableOpacity
+          title="Go back"
+          onPress={() => this.props.navigation.navigate()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22,   
+    paddingTop: 22,
+    backgroundColor: 'green',
+    flex: 1   
   },
 
   item: {
     flex: 1,
     margin: 10,
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
     fontSize: 18,
     textAlign: 'center',
     textAlignVertical: 'center'
-
     },
 })
